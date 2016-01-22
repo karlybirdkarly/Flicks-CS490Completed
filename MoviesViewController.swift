d1fecf45883bb3c8d23c5bb100fe098ae3671845
Karlygash Zhuginissova <karlygash1510@gmail.com>
2016-01-22 14:24:04 -0500

    second commit

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -14,17 +14,13 @@ import EZLoadingActivity
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet var collectionView: UICollectionView!
-    @IBOutlet weak var searchBar: UISearchBar!
-   
-    @IBOutlet weak var networkErrorView: UIView!
-    
     @IBOutlet var tableView: UITableView!
     
-  
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var networkErrorView: UIView!
     
     var collectionViewDisplayed: Bool = false
     
-  
     //var moviesArray = [Movie]()
     // var filteredMovies = [Movie]()
     
@@ -32,61 +28,54 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     var filteredMovies: [NSDictionary]?
     
     var refreshControl: UIRefreshControl!
-     var button: UIButton = UIButton(type: UIButtonType.Custom)
-    
+    var button: UIButton = UIButton(type: UIButtonType.Custom)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchBar.tintColor = UIColor(red: 241.0/255.0, green: 196.0/255.0, blue: 15.0/255.0, alpha: 1.0)
-       
-        //set image for button
+        //image for button
         button.setImage(UIImage(named: "collection.png"), forState: UIControlState.Normal)
-        //add function for button
+        //function for button
         button.addTarget(self, action: "changeViewClicked", forControlEvents: UIControlEvents.TouchUpInside)
-        //set frame
+        //frame
         button.frame = CGRectMake(0, 0, 30, 30)
-        
         let barButton = UIBarButtonItem(customView: button)
-        
-        //barButton.setBackgroundImage(UIImage(named: "collection.png"), forState: .Normal, barMetrics: .Default)
-        //assign button to navigationbar
+        //leftbarbuttonitem is custom button(barbutton)
         self.navigationItem.leftBarButtonItem = barButton
         
+        //MARK: - navigation bar settings
+        navigationController?.navigationBar.barTintColor = UIColor.blackColor()
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 241.0/255.0, green: 196.0/255.0, blue: 15.0/255.0, alpha: 1.0), NSFontAttributeName: UIFont(name: "KohinoorBangla-Semibold", size: 20.0)!]
+        print(navigationController?.navigationBar.titleTextAttributes)
+        navigationItem.title = "FLICKS"
         
+        searchBar.tintColor = UIColor(red: 241.0/255.0, green: 196.0/255.0, blue: 15.0/255.0, alpha: 1.0)
+        
+        //MARK: - refresh control settings
+        refreshControl = UIRefreshControl()
+        refreshControl.backgroundColor = UIColor.blackColor()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        //dismissKeyboard when user taps screen
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
-     
-        filteredMovies = movies
+        
         self.collectionView.hidden = true
+        networkErrorView.hidden = true
+        
+        //MARK: - data sourses and delegates for collection view and table view
         tableView.dataSource = self
         tableView.delegate = self
-        
         collectionView.dataSource = self
         collectionView.delegate = self
-        
-        navigationController?.navigationBar.barTintColor = UIColor.blackColor()
-        
-        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 241.0/255.0, green: 196.0/255.0, blue: 15.0/255.0, alpha: 1.0), NSFontAttributeName: UIFont(name: "KohinoorBangla-Semibold", size: 20.0)!]
-               print(navigationController?.navigationBar.titleTextAttributes)
-        
-                 navigationItem.title = "FLICKS"
-        
-//        let nipName=UINib(nibName: "MovieCellNib", bundle:nil)
-//        
-//        collectionView.registerNib(nipName, forCellWithReuseIdentifier: "CollectionMovieCell")
-        
         searchBar.delegate = self
         
-//        self.tableView.frame = self.view.bounds
-//        self.view.addSubview(self.tableView)
-
-//        Then in your button callback, swap the views out:
-//        
-
-       // collectionView.hidden = true
         
-        //collectionView.removeFromSuperview()
+        filteredMovies = movies
+        
+//        let nipName=UINib(nibName: "MovieCellNib", bundle:nil)
+//        collectionView.registerNib(nipName, forCellWithReuseIdentifier: "CollectionMovieCell")
 
 //        refreshControl.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
 //        actInd.color = UIColor.blackColor()
@@ -94,32 +83,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 //        actInd.startAnimating()
 //        self.tableVIew.tableFooterView = footerView
         
-        //tableView.tableHeaderView?.backgroundColor = UIColor.blackColor()
-        
-        networkErrorView.hidden = true
-        
-        
-        
-//        let leftNavBarButton = UIBarButtonItem(customView:searchBar)
-//        self.navigationItem.rightBarButtonItem = leftNavBarButton
-        
 //        searchBar.frame.origin.y  = (navigationController?.navigationBar.frame.height)!
 //        tableView.frame.origin.y = (navigationController?.navigationBar.frame.height)! + searchBar.frame.height
         
         
         //EZLoadingActivity.showWithDelay("Loading movies...", disableUI: false, seconds: 2)
         //EZLoadingActivity.show("Loading", disableUI: true)
-        refreshControl = UIRefreshControl()
-        refreshControl.backgroundColor = UIColor.blackColor()
-        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
-        tableView.insertSubview(refreshControl, atIndex: 0)
         
 //        var refreshControlViewFrame = self.tableView.bounds;
 //        refreshControlViewFrame.origin.y = -refreshControlViewFrame.size.height
 //        let refreshControlView = UIView(frame: refreshControlViewFrame)
 //        refreshControlView.backgroundColor = UIColor.blackColor()
-        // Do any additional setup after loading the view.
         
+        //MARK: - Parse data using API
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
@@ -128,7 +104,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
             delegate:nil,
             delegateQueue:NSOperationQueue.mainQueue()
-         
         )
         
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
@@ -255,15 +230,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     
 //MARK: - CollectionView
-    
    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //return 20
         return filteredMovies?.count ?? 0
         print("I am displayed")
     }
     
-    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionMovieCell", forIndexPath: indexPath) as! CollectionMovieCell
         
@@ -273,9 +245,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         if let posterPath = movie["poster_path"] as? String {
             let imageUrl = NSURL(string: baseUrl + posterPath)
             let imageRequest = NSURLRequest(URL: imageUrl!)
-            
-            
-            
+
             cell.posterImage.setImageWithURLRequest(imageRequest, placeholderImage: UIImage(named: "placeholder,jpg"), success: {(imageRequest, imageResponse, image) -> Void in
                 
                 if imageResponse != nil {
@@ -284,7 +254,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     cell.posterImage.image = image
                     UIView.animateWithDuration(2.0, animations: {() -> Void in
                         cell.posterImage.alpha = 1.0
-                       
                     })
                     
                 } else {
@@ -294,19 +263,44 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 }, failure: { (imageRequest, imageResponse, error) -> Void in
                     cell.posterImage.image = UIImage(named: "placeholder.jpg")
             })
-            
             cell.posterImage.setImageWithURL(imageUrl!)
         } else {
             cell.posterImage.image = UIImage(named: "placeholder.jpg")
         }
-
-       // print(cell)
         return cell
+    }
+    
+    //MARK: - Switch between CollectionView and TableView
+    func changeViewClicked() {
         
+        var fromView, toView: UIView!
+        if (self.collectionView.hidden) {
+            fromView = self.tableView
+            toView = self.collectionView
+            fromView.hidden = true
+            toView.hidden = false
+            
+            button.setImage(UIImage(named: "table.png"), forState: UIControlState.Normal)
+            
+            UIView.transitionFromView(fromView, toView: toView, duration: 0.75, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+        }
+            
+        else if (self.tableView.hidden == true) {
+            fromView = self.collectionView
+            toView = self.tableView
+            fromView.hidden = true
+            toView.hidden = false
+            
+            button.setImage(UIImage(named: "collection.png"), forState: UIControlState.Normal)
+            
+            UIView.transitionFromView(fromView, toView: toView, duration: 0.75, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+            }
+        print(self.tableView.hidden)
+        self.view.addSubview(searchBar)
     }
 
 
-//MARK: - Refresh
+    //MARK: - Refresh
     func delay(delay:Double, closure:()->()) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay*Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
     }
@@ -316,87 +310,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             self.refreshControl.endRefreshing()
         })
     }
-    
+    //MARK: - Dismiss keyboard
+    func dismissKeyboard() {
+        searchBar.endEditing(true)
+    }
+    //MARK: - Status bar style
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent;
     }
-    
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        searchBar.endEditing(true)
-    }
-    
-    func changeViewClicked() {
-      
-        var fromView, toView: UIView!
-//        print(self.collectionView.superview == self.tableView.superview)
-        if (self.collectionView.hidden) {
-            fromView = self.tableView
-            toView = self.collectionView
-            fromView.hidden = true
-            toView.hidden = false
-            UIView.transitionFromView(fromView, toView: toView, duration: 0.75, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
-            button.setImage(UIImage(named: "table.png"), forState: UIControlState.Normal)
-            
-//            UIView.animateWithDuration(3.5, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-//                self.button.alpha = 1.0
-//                }, completion: nil)
-
-            
-//            print(self.view)
-            //print(toView)
-            
-        }
-            
-        else if (self.tableView.hidden == true)
-        {
-//            print("From view:   \n", fromView)
-            
-            fromView = self.collectionView
-            toView = self.tableView
-            fromView.hidden = true
-            toView.hidden = false
-            UIView.transitionFromView(fromView, toView: toView, duration: 0.75, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
-            
-            button.setImage(UIImage(named: "collection.png"), forState: UIControlState.Normal)
-            
-//            
-//            UIView.animateWithDuration(3.5, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-//                self.button.alpha = 1.0
-//                }, completion: nil)
-            
-           
-        }
-        
-//
-//        //fromView.hidden = true
-//        
-//        
-       // toView.frame = self.view.bounds
-////
-//        self.view.addSubview(toView)
-//                UIView.transitionFromView(fromView, toView: toView, duration: 0.75, options: UIViewAnimationOptions.TransitionFlipFromRight,
-//                    completion: nil)
-        print(self.tableView.hidden)
-
-//
-        self.view.addSubview(searchBar)
-//
-        }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidAppear(animated: Bool) {
-        UIView.animateWithDuration(3.5, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-                self.button.alpha = 1.0
-                }, completion: nil)
-            
-        
-    }
+//    override func viewDidAppear(animated: Bool) {
+//        UIView.animateWithDuration(3.5, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+//                self.button.alpha = 1.0
+//                }, completion: nil)
+//
+//    }
 
     /*
     // MARK: - Navigation
